baa7439e94ea09bb431302d4a47db61dc58d63fe
Rick Shinners <rickshinners@gmail.com>
2018-04-07 19:42:32 -0400

    consolidate in a single python file

--- Readme.md
+++ Readme.md
@@ -1,12 +1,20 @@
 # DNS-Updater
 This docker container finds the external IP of the current container and updates a configured AWS Route 53 DNS entry.
 
-## Setup
-The image requires a `config.json` file to be mounted in the `/config` mount point.  You can see an example config file at `config.json.example`
+# Usage
+```
+docker create \
+-e AWS_ACCESS_KEY_ID=<AWS_ACCESS_KEY_ID> \
+-e AWS_SECRET_ACCESS_KEY=<AWS_SECRET_ACCESS_KEY> \
+-e HOSTED_ZONE_ID=<HOSTED_ZONE_ID> \
+-e DNS_NAME=<DNS_NAME> \
+rickshinners/dns-updater-docker
+```
 
-## Quick start
-1. `docker build . -t dns-updater`
-2. `mkdir config`
-3. `cp config.json.example config/config.json`
-4. `# Fill out config.json`
-2. `docker run --rm -v ./config:/config dns-updater`
+# Parameters
+* *AWS_ACCESS_KEY_ID* Your AWS access key id
+* *AWS_SECRET_ACCESS_KEY* Your AWS secret access key
+* *HOSTED_ZONE_ID* The AWS hosted zone id that contains the target dns name
+* *DNS_NAME* DNS name to set to the running instance's external IP
+* *CRON* The cron schedule on which to run. Defaults to every 5 minutes
+* *DNS_TTL* TTL in seconds for the DNS record.  Defaults to 300
--- files/update-dns.py
+++ files/update-dns.py
@@ -11,15 +11,86 @@ import os
 import ipgetter
 from crontab import CronTab
 from time import sleep
+from string import Template
+from subprocess import check_output, call
+import logging
 
-required_env = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'HOSTED_ZONE_ID', 'DNS_NAME']
-for env in required_env:
-    if(os.getenv(env) == None):
-        raise Exception("The environment variable %s must be set" % env)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
-# print(ipgetter.myip())
+logger.debug('Startup')
+logger.info("AWS_ACCESS_KEY_ID=%s" % os.getenv('AWS_ACCESS_KEY_ID'))
+logger.info("HOSTED_ZONE_ID=%s" % os.getenv('HOSTED_ZONE_ID'))
+logger.info("DNS_NAME=%s" % os.getenv('DNS_NAME'))
+logger.info("DNS_TTL=%s" % os.getenv('DNS_TTL', 300))
+logger.info("CRON=%s" % os.getenv('CRON', '*/5 * * * *'))
 
-schedule = CronTab(os.getenv('CRON', '* * * * *'))
+def check_required_environment_variables():
+    required_env = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'HOSTED_ZONE_ID', 'DNS_NAME']
+    for env in required_env:
+        if(os.getenv(env) == None):
+            raise Exception("The environment variable %s must be set" % env)
+
+def get_changebatch(dnsname, ipaddress, ttl):
+    template = Template("""
+{
+    "Comment": "Updating $dnsname A record to $ipaddress with $ttl second TTL",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "$dnsname",
+                "Type": "A",
+                "TTL": $ttl,
+                "ResourceRecords": [
+                    {
+                        "Value": "$ipaddress"
+                    }
+                ]
+            }
+        }
+    ]
+}""")
+    d = {'dnsname':dnsname, 'ipaddress': ipaddress, 'ttl': ttl}
+    return template.substitute(d)
+
+def get_current_ip():
+    return check_output(['aws', 'route53',
+        'test-dns-answer',
+        '--hosted-zone-id', os.getenv('HOSTED_ZONE_ID'),
+        '--record-name', os.getenv('DNS_NAME'),
+        '--record-type', 'A',
+        '--query', 'RecordData[0]',
+        '--output', 'text']).decode('utf-8').strip()
+
+def check_dns_and_update():
+    myip = ipgetter.myip()
+    currentip = get_current_ip()
+    logger.debug('current external ip: %s' % myip)
+    logger.debug('%s resolves to %s' %(os.getenv('DNS_NAME'), currentip))
+    if(myip != currentip):
+        changebatch = get_changebatch(os.getenv('DNS_NAME'), myip, os.getenv('DNS_TTL',300))
+        change_message = check_output(['aws', 'route53',
+            'change-resource-record-sets',
+            '--hosted-zone-id', os.getenv('HOSTED_ZONE_ID'),
+            '--change-batch', changebatch,
+            '--query', 'ChangeInfo.Comment',
+            '--output', 'text']).decode('utf-8').strip()
+        logger.info(change_message)
+
+check_required_environment_variables()
+check_dns_and_update()
+
+schedule = CronTab(os.getenv('CRON', '*/5 * * * *'))
 while(True):
-    sleep(schedule.next(default_utc=True))
-    print("Check IP")
+    try:
+        sleep(schedule.next(default_utc=True))
+        check_dns_and_update()
+    except KeyboardInterrupt:
+        logger.debug("Exiting after KeyboardInterrupt")
+        exit(0)
